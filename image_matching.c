75170991fe23006689049c262bfb2534563f7165
ericsia <ericsia12@gmail.com>
2023-10-26 23:06:47 +0800

    Update image_matching.c

--- image_matching.c
+++ image_matching.c
@@ -1,71 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h> // For taking a screenshot on Windows
-
-// Function to load a BMP image into memory
-unsigned char *loadBMP(const char *filename, int *width, int *height)
-{
-    FILE *file = fopen(filename, "rb");
-    if (!file)
-    {
-        fprintf(stderr, "Failed to open file\n");
-        return NULL;
-    }
-
-    // Read BMP header
-    fseek(file, 18, SEEK_SET);
-    fread(width, sizeof(int), 1, file);
-    fseek(file, 22, SEEK_SET);
-    fread(height, sizeof(int), 1, file);
-
-    int widthRGB = *width * 3;
-    int imageSize = widthRGB * *height;
-    unsigned char *data = (unsigned char *)malloc(imageSize);
-    if (!data)
-    {
-        fclose(file);
-        return NULL;
-    }
-
-    int padding = (4 - (widthRGB & 3)) & 3;
-
-    fseek(file, 54, SEEK_SET);
-    for (int i = 0; i < *height; ++i)
-    {
-        fread(data + i * widthRGB, 1, widthRGB, file);
-        fseek(file, padding, SEEK_CUR); // Skip padding bytes
-    }
-
-    fclose(file);
-
-    return data;
-}
-
-int pad(int width)
-{
-    int paddingSize = 0;
-    if ((width * 3) % 4 != 0)
-    {
-        paddingSize = 4 - ((width * 3) % 4);
-    }
-    return (width * 3 + paddingSize);
-}
+#include <stdio.h>   // Include standard input/output library for printing messages
+#include <stdlib.h>  // Include standard library for memory management
+#include <windows.h> // Include the Windows API for screen capture
 
 int main()
 {
-    int cropWidth, cropHeight;
-    unsigned char *croppedArray = loadBMP("crop.bmp", &cropWidth, &cropHeight);
-    int strideCrop = 3 * cropWidth;
+    FILE *log = fopen("log.txt", "w");
 
-    int screenWidth, screenHeight;
-    int strideScreen = 3 * screenWidth;
-    unsigned char *screenArray = loadBMP("screen.bmp", &screenWidth, &screenHeight);
+    // Load the BMP image from Microsoft Paint 24-bit BMP
+    int croppedWidth, croppedHeight;
 
-    int screenXMax = screenWidth - cropWidth;
-    int screenYMax = screenHeight - cropHeight;
+    FILE *file = fopen("cropped.bmp", "rb"); // Open the BMP image file in binary read mode
+    if (!file)                               // Check if the file couldn't be opened
+    {
+        fprintf(log, "Failed to open the cropped image file.\n"); // Print an error message to the standard error stream
+        return 1;                                                 // Return an error code and exit
+    }
+
+    // Read the BMP header to get image dimensions
+    fseek(file, 18, SEEK_SET);                   // Move the file pointer to the width field in the BMP header
+    fread(&croppedWidth, sizeof(int), 1, file);  // Read the width of the image
+    fseek(file, 22, SEEK_SET);                   // Move the file pointer to the height field in the BMP header
+    fread(&croppedHeight, sizeof(int), 1, file); // Read the height of the image
+
+    // Calculate the size of the cropped image
+    int cropWidthRGB = 3 * croppedWidth;                                     // Calculate the size of one row RGB of the image
+    int croppedImageSize = cropWidthRGB * croppedHeight;                     // Calculate the total size of the image needed
+    unsigned char *croppedArray = (unsigned char *)malloc(croppedImageSize); // Allocate memory to store the image
+    if (!croppedArray)                                                       // Check if memory allocation failed
+    {
+        fclose(file);                                                       // Close the image file
+        fprintf(log, "Failed to allocate memory for the cropped image.\n"); // Print an error message
+        return 1;                                                           // Return an error code and exit
+    }
+
+    int cropPadding = (4 - (cropWidthRGB & 3)) & 3; // the extra pad size
+    fprintf(log, "Padding: %d\n", cropPadding);     // debug
+
+    // Read the pixel data of the cropped image
+    fseek(file, 54, SEEK_SET);              // Move the file pointer to the start of the pixel data
+    for (int i = 0; i < croppedHeight; ++i) // Read the pixel data into the allocated memory
+    {
+        fread(croppedArray + i * cropWidthRGB, 1, cropWidthRGB, file);
+        fseek(file, cropPadding, SEEK_CUR); // Skip padding bytes
+    }
+    fclose(file); // Close the image file
+
+    // Capture a 1920x1080 screenshot
+    int screenWidth = 1920;
+    int screenHeight = 1080;
+    HDC hScreenDC = GetDC(NULL); // Get a handle to the screen device context
+
+    // Create a compatible DC and bitmap to store the screenshot
+    HDC hMemoryDC = CreateCompatibleDC(hScreenDC);                                        // Create a compatible device context
+    HBITMAP hBitmapScreen = CreateCompatibleBitmap(hScreenDC, screenWidth, screenHeight); // Create a compatible bitmap
+    SelectObject(hMemoryDC, hBitmapScreen);                                               // Select the bitmap into the device context
+    BitBlt(hMemoryDC, 0, 0, screenWidth, screenHeight, hScreenDC, 0, 0, SRCCOPY);         // Copy the screen content to the bitmap
+
+    // Get the pixel data of the screenshot
+    BITMAPINFOHEADER bmiHeader;
+    bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmiHeader.biWidth = screenWidth;
+    bmiHeader.biHeight = screenHeight; // Use a positive value
+    bmiHeader.biPlanes = 1;
+    bmiHeader.biBitCount = 24;
+    bmiHeader.biCompression = BI_RGB;
+
+    int screenImageSize = screenWidth * 3 * screenHeight;                                                            // Calculate the size of RGB of the screenshot
+    unsigned char *screenshotArray = (unsigned char *)malloc(screenImageSize);                                       // Allocate memory to store the screenshot
+    GetDIBits(hScreenDC, hBitmapScreen, 0, screenHeight, screenshotArray, (BITMAPINFO *)&bmiHeader, DIB_RGB_COLORS); // Retrieve the pixel data
+
+    // Clean up screenshot resources
+    DeleteObject(hBitmapScreen); // Delete the bitmap
+    DeleteDC(hMemoryDC);         // Delete the device context
+    ReleaseDC(NULL, hScreenDC);  // Release the screen device context
+
+    // Find the exact match
+    int screenXMax = screenWidth - croppedWidth;
+    int screenYMax = screenHeight - croppedHeight;
     int matchX = -1;
     int matchY = -1;
-    printf("%d %d\n", screenXMax, screenYMax);
 
     // Iterate through the screenshot and find the matching region
     for (int screenY = 0; screenY <= screenYMax; screenY++)
@@ -74,12 +87,12 @@ int main()
         {
             int match = 1;
             // Compare pixel blocks using memcmp for faster comparison
-            for (int croppedY = 0; croppedY < cropHeight; croppedY++)
+            for (int croppedY = 0; croppedY < croppedHeight; croppedY++)
             {
-                int offsetCropped = strideCrop * croppedY;                             // Calculate the starting position in the cropped image
+                int offsetCropped = cropWidthRGB * croppedY;                           // Calculate the starting position in the cropped image
                 int offsetScreen = 3 * ((screenY + croppedY) * screenWidth + screenX); // Calculate the starting position in the screenshot
                 // Compare entire pixel blocks using memcmp
-                if (memcmp(&screenArray[offsetScreen], &croppedArray[offsetCropped], strideCrop) != 0)
+                if (memcmp(&screenshotArray[offsetScreen], &croppedArray[offsetCropped], cropWidthRGB) != 0)
                 {
                     match = 0; // Not a match
                     break;
@@ -88,8 +101,8 @@ int main()
 
             if (match)
             {
-                matchX = screenX;                             // Record the x-coordinate of the match
-                matchY = screenHeight - cropHeight - screenY; // Record the y-coordinate of the match
+                matchX = screenX;                                // Record the x-coordinate of the match
+                matchY = screenHeight - croppedHeight - screenY; // Record the y-coordinate of the match
                 break;
             }
         }
@@ -102,7 +115,7 @@ int main()
 
     // Save match coordinates to a file
     FILE *matchFile = fopen("match_coordinates.txt", "w"); // Open a file to write the match coordinates
-    if (matchX >= 0)
+    if (matchX != -1)
     {
         fprintf(matchFile, "Image match found at (%d, %d)\n", matchX, matchY); // Print the coordinates to the file
     }
@@ -111,6 +124,11 @@ int main()
         fprintf(matchFile, "Image match not found.\n"); // Print a message indicating no match
     }
     fclose(matchFile); // Close the file
+    fclose(log);       // Close the file
 
-    return 0;
+    // Clean up memory
+    free(croppedArray);    // Free the memory allocated for the cropped image
+    free(screenshotArray); // Free the memory allocated for the screenshot
+
+    return 0; // Exit the program
 }
